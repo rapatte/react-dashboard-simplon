eef52c5b7299bf4dd0380392caad4c405f2341a9
antoine <antoine.gayoso@gmail.com>
2021-06-29 13:46:38 +0200

    feat: navbar refacto function + display drawer when isAuth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import {
     AppBar,
     Toolbar,
@@ -13,65 +13,59 @@ import MenuIcon from "@material-ui/icons/Menu";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import { Link, withRouter } from "react-router-dom";
+import { AuthContext } from "../store";
 
-class NavBar extends React.Component {
+const NavBar = () => {
+    const { isAuth, setIsAuth } = useContext(AuthContext);
+    const [drawer, setDrawer] = useState(false);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            drawer: false
-        };
+    const toggleDrawer = () => {
+        setDrawer(!drawer)
     }
 
-    toggleDrawer = () => {
-        this.setState({
-            drawer: !this.state.drawer
-        });
-    }
-
-    logout = () => {
+    const logout = () => {
         // todo use authcontext logout method
-        this.toggleDrawer();
+        toggleDrawer();
+        localStorage.clear();
+        setIsAuth(false);
     }
 
-    render() {
-        return (
-            <div>
-                <AppBar position="static" >
-                    <Toolbar style={{ display: "flex", justifyContent: "space-between"}}>
-                        <IconButton color="inherit" onClick={this.toggleDrawer}>
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6">
-                            React Dashboard
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+    return (
+        <div>
+            <AppBar position="static" >
+                <Toolbar style={{ display: "flex", justifyContent: "space-between"}}>
+                    {isAuth ? <IconButton color="inherit" onClick={toggleDrawer}>
+                        <MenuIcon />
+                    </IconButton> : null}
+                    <Typography variant="h6">
+                        React Dashboard
+                    </Typography>
+                </Toolbar>
+            </AppBar>
                 <Drawer
-                    open={this.state.drawer}
-                    onClose={this.toggleDrawer}
-                    style={{ width: 600 }}
-                >
-                    <List>
-                        <ListItem>
-                            <Link to="/">
-                                <Button color="secondary" onClick={this.logout}>
-                                    <ExitToAppIcon /> Logout
-                                </Button>
-                            </Link>
-                        </ListItem>
-                        <ListItem>
-                            <Link to="/dashboard">
-                                <Button color="primary" onClick={this.toggleDrawer}>
-                                    <DashboardIcon /> Dashboard
-                                </Button>
-                            </Link>
-                        </ListItem>
-                    </List>
-                </Drawer>
-            </div>
-        );
-    }
+                open={drawer}
+                onClose={toggleDrawer}
+                style={{ width: 600 }}
+            >
+                <List>
+                    <ListItem>
+                        <Link to="/">
+                            <Button color="secondary" onClick={logout}>
+                                <ExitToAppIcon /> Logout
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link to="/dashboard">
+                            <Button color="primary" onClick={toggleDrawer}>
+                                <DashboardIcon /> Dashboard
+                            </Button>
+                        </Link>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </div>
+    );
 }
+
 export default withRouter(NavBar);
